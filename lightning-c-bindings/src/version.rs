42c265c7a9e10d4f7d452a1ead812562eae7e092
Matt Corallo <git@bluematt.me>
2021-07-09 18:49:59 +0000

    Check-in version.rs as a testcase!

---
+++ lightning-c-bindings/src/version.rs
@@ -0,0 +1,8 @@
+#[no_mangle]
+pub extern "C" fn _ldk_get_compiled_version() -> crate::c_types::Str {
+	"v0.0.98-91-ge04a1b387230c9fd".into()
+}
+#[no_mangle]
+pub extern "C" fn _ldk_c_bindings_get_compiled_version() -> crate::c_types::Str {
+	"v0.0.98.1-8-ga9dfc1bf8c793b40".into()
+}
